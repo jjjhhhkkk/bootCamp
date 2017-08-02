482ad1334488ec32d27b788855db43ecf7f2c410
wider <wider@LAPTOP-CT8T8F8O>
2017-08-02 14:15:50 +0900

    jpa_1

--- src/main/java/com/apress/spring/domain/Journal.java
+++ src/main/java/com/apress/spring/domain/Journal.java
@@ -10,15 +10,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
-import lombok.Data;
-
-
 @Entity
-@Data
 public class Journal {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String title;
     private Date created;
@@ -27,18 +23,47 @@ public class Journal {
     @Transient
     private SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
 
-    public Journal(String title, String summary, String date)
-            throws ParseException {
+    public Journal(String title, String summary, String date) throws ParseException{
         this.title = title;
         this.summary = summary;
         this.created = format.parse(date);
     }
 
-    Journal() {
+    Journal() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
     }
 
     public String getCreatedAsShort() {
         return format.format(created);
     }
-
 }
